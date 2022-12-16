92ce51ab14ebaa75b2a885430d7c27fa3d92277d
theomelgar <theomelgar@gmail.com>
2022-12-16 14:29:08 -0300

    Add habit/ Today

--- src/components/Habit.js
+++ src/components/Habit.js
@@ -1,11 +1,23 @@
+import { useContext } from "react"
 import styled from "styled-components"
+import { ListContext } from "../context/list"
 
-export default function Habit({setEmpty}){
+export default function Habit(){
+    const {setEmpty} = useContext(ListContext)
     return(
         <StyleHabit>
-
+            <p>Terminar o projeto</p>
+            <ion-icon name="trash-outline"></ion-icon>
+            
         </StyleHabit>
     )
 }
 const StyleHabit = styled.div`
+width: 340px;
+height: 91px;
+left: 17px;
+top: 147px;
+
+background: #FFFFFF;
+border-radius: 5px;
 `
