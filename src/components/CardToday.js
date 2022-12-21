2ffcd655a2792cba2b2b20a735cadc3082906954
theomelgar <theomelgar@gmail.com>
2022-12-20 22:31:54 -0300

    load checks

--- src/components/CardToday.js
+++ src/components/CardToday.js
@@ -1,50 +1,62 @@
 import { useState } from "react";
 import styled from "styled-components";
 import { api } from "../services/auth";
+import Loading from "./Loading";
 
 export default function CardToday({ activity, token, setUpdate }) {
     const [click, setClick] = useState(true)
     const { id, name, currentSequence, highestSequence, done } = activity
+    const [load, setLoad] = useState(
+        <ion-icon name="checkmark-sharp"></ion-icon>
+    )
     const config = {
         headers: {
             Authorization: `Bearer ${token}`
         }
     }
     function check() {
+        setLoad(Loading)
         api
             .post(`habits/${id}/check`, {}, config)
             .then(() => {
                 setClick(!click)
                 setUpdate(up => !up)
+                setLoad(<ion-icon name="checkmark-sharp"></ion-icon>)
             }
             )
-            .catch(err => alert(err.response.data.message))
+            .catch(err => {alert(err.response.data.message)
+                setLoad(<ion-icon name="checkmark-sharp"></ion-icon>)
+            })
     }
     function unCheck() {
+        setLoad(Loading)
         api
             .post(`habits/${id}/uncheck`, {}, config)
             .then(() => {
                 setClick(!click)
                 setUpdate(up => !up)
+                setLoad(<ion-icon name="checkmark-sharp"></ion-icon>)
             }
             )
-            .catch(err => alert(err.response.data.message))
+            .catch(err => {alert(err.response.data.message)
+                setLoad(<ion-icon name="checkmark-sharp"></ion-icon>)
+            })
     }
-    
+
     return (
         <StyleToday data-test="today-habit-container">
             <p data-test="today-habit-name">{name}</p>
-            
+
             <Streak color={currentSequence === highestSequence ? "#8FC549" : "#666666"}>
-                <p data-test="today-habit-sequence" >Sequência atual:<span>{currentSequence} {currentSequence>1 ? "dias" : "dia"}</span> </p>
-                <p data-test="today-habit-record">Seu recorde:<span>{highestSequence} {currentSequence>1 ? "dias" : "dia"}</span> </p>
+                <p data-test="today-habit-sequence" >Sequência atual:<span>{currentSequence} {currentSequence > 1 ? "dias" : "dia"}</span> </p>
+                <p data-test="today-habit-record">Seu recorde:<span>{highestSequence} {currentSequence > 1 ? "dias" : "dia"}</span> </p>
             </Streak>
             {!done ? (
                 <Check data-test="today-habit-check-btn" onClick={check} cor="#EBEBEB">
-                    <ion-icon name="checkmark-sharp"></ion-icon>
+                    {load}
                 </Check>) :
                 (<Check data-test="today-habit-check-btn" onClick={unCheck} cor="#8FC549">
-                    <ion-icon name="checkmark-sharp"></ion-icon>
+                    {load}
                 </Check>)}
         </StyleToday>
     )
@@ -76,6 +88,9 @@ const Streak = styled.div`
 const Check = styled.div`
     box-sizing: border-box;
     position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     width: 69px;
     height: 69px;
     right: 10px;
@@ -85,7 +100,7 @@ const Check = styled.div`
     color: white;
     background: ${props => props.cor};
     border: 1px solid #E7E7E7;
-    border-radius: 5px;
+    border-radius: 10px;
     cursor: pointer;
     &:hover{
         transform: scale(1.1);
